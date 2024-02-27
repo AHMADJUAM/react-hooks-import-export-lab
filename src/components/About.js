954fbcc50afb27e2f0a78866ba2a6dc9b7774994
ahmad juma <juam17020@gmail.com>
2024-02-27 03:48:04 +0300

    Completed assignment

--- src/components/About.js
+++ src/components/About.js
@@ -1,4 +1,18 @@
+// import React from "react";
+
+// function About() {
+//   return (
+//     <div id="about">
+//       <h2>About Me</h2>
+//       <p>I Made This</p>
+//       <img src={image} alt="I made this" />
+//     </div>
+//   );
+// }
+// export default About;
+
 import React from "react";
+import { image } from "../data/user";
 
 function About() {
   return (
@@ -9,3 +23,4 @@ function About() {
     </div>
   );
 }
+export default About;
